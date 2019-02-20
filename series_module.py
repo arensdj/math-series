630b1d8e390013ec25eb12e5852e1454a52c6203
Jasmin <jasmin.arensdorf@gmail.com>
2019-02-19 20:15:58 -0800

    Coding lucas function.

--- series_module.py
+++ series_module.py
@@ -1,16 +1,57 @@
 def fibonacci(n):
-  # n = int(input())
   array = []
   index = 1
   array.append(index)
   array.append(index)
   total = 0
 
-  while (index < n):
-    total = array[index -1] + array[index]
+  for i in range(index, n):
+    total = array[i - 1] + array[i]
     array.append(total)
-    index += 1
+    
+  return array[i]
 
-  print(str(array[index - 1]))
+def lucas(n):
+  array = []
+  index = 1
+  array.append(index+1)
+  array.append(index)
+  total = 0
 
-  return array
+  for i in range(index, n):
+  # while (index < n):
+    total = array[i -1] + array[i]
+    array.append(total)
+    # index += 1
+  
+  print(str(array))
+  print(str(array[i]))
+  return array[i]
+
+
+  # while (index < n):
+  #   total = array[index -1] + array[index]
+  #   array.append(total)
+  #   index += 1
+
+  # print(str(array))
+  # print(str(array[index-1]))
+  # return array[index-1]
+
+# def sum_series(n, ):
+
+
+
+n = 5
+# result = fibonacci(n)
+# print('Returned: ' + str(result))
+result = lucas(n)
+
+
+
+# while (index < n):
+    # total = array[index -1] + array[index]
+    # index += 1
+    # i += 1
+# print('Content: ' + str(array))
+# return array[index-1]
--- test_series.py
+++ test_series.py
@@ -2,16 +2,29 @@
   This is a test function to test fibonnaci program 
 """
 from series_module import fibonacci
+from series_module import lucas
 
 def test_fib_series():
   """
     This is a test function to test fibonnaci program 
       
-   Attributes: 
+    Attributes: 
       n: A numeric value  
   """
   n = 6
   actual = fibonacci(n)
   expected = 8
+  assert expected == actual
+
+def test_lucas_series():
+  """
+    This is a test function to test fibonnaci program 
+      
+  Attributes: 
+      n: A numeric value  
+  """
+  n = 5
+  actual = lucas(n)
+  expected = 7
   assert expected == actual
 
